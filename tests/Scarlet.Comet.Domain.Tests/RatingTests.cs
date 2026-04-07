8a2cbc066f28bcdfb5a5b7c064f464aea99cede4
York Fan <fanyuke4925@gmail.com>
2026-04-07 18:12:58 -0400

    Complete Lab 5 backend tests

---
+++ tests/Scarlet.Comet.Domain.Tests/RatingTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Scarlet.Comet.Domain.Catalog;
+using System;
+
+namespace Scarlet.Comet.Domain.Tests
+{
+    [TestClass]
+    public class RatingTests
+    {
+        [TestMethod]
+        public void Can_Create_New_Rating()
+        {
+            var rating = new Rating(1, "Mike", "Great fit!");
+
+            Assert.AreEqual(1, rating.Stars);
+            Assert.AreEqual("Mike", rating.UserName);
+            Assert.AreEqual("Great fit!", rating.Review);
+        }
+
+        [TestMethod]
+        public void Cannot_Create_Rating_With_Invalid_Stars()
+        {
+            try
+            {
+                var rating = new Rating(0, "Mike", "Great fit!");
+                Assert.Fail("Expected an exception to be thrown.");
+            }
+            catch (ArgumentException)
+            {
+                // test passed
+            }
+        }
+    }
+}
